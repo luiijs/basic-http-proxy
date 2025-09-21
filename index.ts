d58b825c67c8e1271c3203afcd01fade79d7d181
Luii <altintasaenes@gmail.com>
2025-09-21 11:29:49 +0300

    Create

---
+++ index.ts
@@ -0,0 +1,13 @@
+import fastify from "fastify";
+import fastifyHttpProxy from "@fastify/http-proxy";
+
+const Fastify = fastify({
+    logger: true
+})
+
+await Fastify.register(fastifyHttpProxy, {
+    upstream: "https://google.com",
+    prefix: "/name"
+})
+
+Fastify.listen({ port: 4000, host: '0.0.0.0' })
